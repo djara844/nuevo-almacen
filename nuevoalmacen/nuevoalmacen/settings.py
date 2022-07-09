b63d7ce2d8e79030753027fe27c7a5977acdf0df
Diego Jaramillo <djaramillo@vibra.fm>
2022-07-09 11:31:17 -0500

    Update gitignore and create conection with database postgresql

--- nuevoalmacen/nuevoalmacen/settings.py
+++ nuevoalmacen/nuevoalmacen/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "nuevoalmacen.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbnuevoalmacen', 
+        'USER': 'djara844', 
+        'PASSWORD': 'Tankard1946*',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
